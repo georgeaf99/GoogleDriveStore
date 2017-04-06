12d06b3571c799cf43b15eac859cf1fff0fada9b
George Farcasiu <farcasiu.george@gmail.com>
2017-04-06 17:32:38 -0500

    Mocked out the basic interface for store

---
+++ store.py
@@ -0,0 +1,12 @@
+class Store:
+    def __init__(self, spreadsheet_name, credentials):
+        pass
+    
+    def get(self, index):
+        pass
+
+    def push(self, data):
+        pass
+
+    def update(self, index, data):
+        pass
