70e38f092386fa175086d1d6f49e13e640af3fd7
George Farcasiu <farcasiu.george@gmail.com>
2017-04-06 18:16:29 -0500

    Fleshed out init

--- store.py
+++ store.py
@@ -1,7 +1,10 @@
+import gspread
+
 class Store:
-    def __init__(self, spreadsheet_name, credentials):
-        pass
-    
+    def __init__(self, spreadsheet_id, credentials):
+        self._gspread_client = gspread.authorize(credentials)
+        self._spreadsheet = self._gspread_client.open_by_key(spreadsheet_id)
+
     def get(self, index):
         pass
 
@@ -10,3 +13,9 @@ class Store:
 
     def update(self, index, data):
         pass
+
+if __name__ == "__main__":
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
+    store = Store("foobar", credentials)
